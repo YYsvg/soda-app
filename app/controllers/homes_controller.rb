94e9d77645234049b1cce67479aab4b80271ebbb
YYsvg <rara10202001@gmail.com>
2024-05-04 12:03:45 +0900

    create_each_user_view

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,18 +2,21 @@ class HomesController < ApplicationController
   before_action :authenticate
   
   def index
+
+    @users = User.all
+
+    @income_categories = IncomeCategory.all
+    @outcome_categories = OutcomeCategory.all
+
     @income = Income.new
     @outcome = Outcome.new
     @want = Want.new
-    
+
     @income_category = IncomeCategory.new
     @outcome_category = OutcomeCategory.new
 
     @incomename = "収入"
     @outcomename = "支出"
-
-    @income_categories = IncomeCategory.all
-    @outcome_categories = OutcomeCategory.all
 
   end
 end
